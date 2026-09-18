fcd8432bf85937dd314b894c1c361b45c028f6af
xixi-lcn <azralcn33@gmail.com>
2026-09-18 11:21:44 +0300

    Leaderboard

--- Assets/Scripts/Systems/Leaderboard.cs
+++ Assets/Scripts/Systems/Leaderboard.cs
@@ -8,7 +8,7 @@ public class PlayerScore
     public string name;
     public float time;
 
-    public PlayerScore(string name,float time)
+    public PlayerScore(string name, float time)
     {
         this.name = name;
         this.time = time;
@@ -16,8 +16,12 @@ public class PlayerScore
 }
 public class Leaderboard : MonoBehaviour
 {
-    public List<PlayerScore> players =new List<PlayerScore>();
+    public List<PlayerScore> players = new List<PlayerScore>();
 
+    private void Start()
+    {
+        LoadPlayers();
+    }
 
     public bool AddPlayer(string name, float time)
     {
@@ -30,13 +34,46 @@ public class Leaderboard : MonoBehaviour
         }
         players.Add(new PlayerScore(name, time));
         SortLeaderboard();
+        SavePlayers();
         return true;
     }
+
 
     private void SortLeaderboard()
     {
         players.Sort((a, b) => {
             return a.time.CompareTo(b.time);
-            });
+        });
+    }
+
+    private void SavePlayers()
+    {
+        PlayerPrefs.SetInt("PlayerCount", players.Count);
+        for (int i = 0; i < players.Count; i++)
+        {
+            PlayerPrefs.SetString("PlayerName_" + i, players[i].name);
+            PlayerPrefs.SetFloat("PlayerTime_" + i, players[i].time);
+        }
+
+        PlayerPrefs.Save();
+    }
+    private void LoadPlayers()
+    {
+        players.Clear();
+
+        int playerCount = PlayerPrefs.GetInt("PlayerCount", 0);
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            string name = PlayerPrefs.GetString("PlayerName_" + i, "");
+            float time = PlayerPrefs.GetFloat("PlayerTime_" + i, 0f);
+
+            if (name != "")
+            {
+                players.Add(new PlayerScore(name, time));
+            }
+
+        }
+        SortLeaderboard();
     }
 }
--- Assets/Scripts/Systems/LeaderboardUI.cs
+++ Assets/Scripts/Systems/LeaderboardUI.cs
@@ -7,24 +7,45 @@ public class LeaderboardUI : MonoBehaviour
     private TMP_InputField inputName;
     [SerializeField]
     private Leaderboard leaderboard;
-   
+
+    public TextMeshProUGUI winnerText;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI timeText;
+
     public void SaveScore()
     {
         string name = inputName.text;
-        if (name=="")
+        if (name == "")
         {
             return;
         }
         TimeManager timeManager = FindFirstObjectByType<TimeManager>();
 
-        if (leaderboard.AddPlayer(name,timeManager.timePassed))
+        if (leaderboard.AddPlayer(name, timeManager.timePassed))
         {
             ShowLeaderboard();
         }
     }
-
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && inputName.isFocused)
+        {
+            SaveScore();
+        }
+    }
     public void ShowLeaderboard()
     {
+        winnerText.text = "";
+        nameText.text = "";
+        timeText.text = "";
+
+        for (int i = 0; i < leaderboard.players.Count; i++)
+        {
+            winnerText.text += (i + 1).ToString() + "\n\n";
+            nameText.text += leaderboard.players[i].name + "\n\n";
+            timeText.text += leaderboard.players[i].time.ToString("F2") + "\n\n";
+
+        }
 
     }
 
